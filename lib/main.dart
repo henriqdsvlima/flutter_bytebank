2998d5d917021eef68fe6bcb6c2b6f2d0a3d85b5
FlutLab.io <>
2021-04-28 17:57:43 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,17 @@ import 'package:flutter/src/material/list_tile.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        body: Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on),
-            title: Text('199 reais'),
-            subtitle: Text('2000'),
-          ), //ListTile
-        ), //Card
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('199 reais'),
+                subtitle: Text('2000'),
+              ), //ListTile
+            ), //Card
+          ], //<Widget>[]
+        ), //Column
         appBar: AppBar(
           title: Text('Transferências'),
         ), //AppBar
