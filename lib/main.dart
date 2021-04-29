bfe6606bfb8ec22f8d7a4b0ef3d8f5663abcc303
FlutLab.io <>
2021-04-29 17:10:48 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class TransferForm extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text('Criando Transferências'),
-         backgroundColor: Color(#25D366),
+         backgroundColor: Color(25D366),
         ),
         body: Column());
   }
